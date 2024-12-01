5c29703d668d872ff826d3a8db967f72dbd0e112
LemonNeko <chheese048@gmail.com>
2024-12-02 00:20:44 +0800

    allow pause, step by step, reset, regenerate, show steps

--- src/components/SelectionSort.vue
+++ src/components/SelectionSort.vue
@@ -1,20 +1,29 @@
 <script lang="ts" setup>
-import { ref, Ref, watch } from 'vue';
+import { ref, watch } from 'vue';
 
 const smallest = ref(0);
 const current = ref(0);
 const numbers = ref<{ num: number, order: number }[]>([])
-const outerPosition = ref(0);
-const innerPosition = ref(0);
-const paused = ref(true);
+const originalNumbers = ref<{ num: number, order: number }[]>([]);
+const started = ref(false);
+const paused = ref(false);
+const stepByStep = ref(false);
 const showSwapAnimation = ref(true);
+const needReset = ref(false);
+const stepsCount = ref(0);
 
-for (let i = 0; i < 10; i++) {
-    numbers.value.push({
-        num: Math.floor(Math.random() * 100),
-        order: i
-    });
+function generateRandomNumbers() {
+    numbers.value = [];
+    for (let i = 0; i < 10; i++) {
+        numbers.value.push({
+            num: Math.floor(Math.random() * 100),
+            order: i
+        });
+    }
+
+    originalNumbers.value = numbers.value.map(n => ({ ...n }));
 }
+generateRandomNumbers()
 
 async function sleep(ms: number) {
     return new Promise<void>((resolve) => {
@@ -22,7 +31,7 @@ async function sleep(ms: number) {
     });
 }
 
-async function waitUntil(condition: Ref<boolean>) {
+async function waitUntil(condition: () => boolean) {
     return new Promise<void>((resolve) => {
         const handle = watch([condition], (val) => {
             if (val) {
@@ -33,22 +42,92 @@ async function waitUntil(condition: Ref<boolean>) {
     });
 }
 
+function reset() {
+    numbers.value = originalNumbers.value.map(n => ({ ...n }));
+    smallest.value = 0;
+    current.value = 0;
+    started.value = false;
+    paused.value = false;
+    stepByStep.value = false;
+    showSwapAnimation.value = true;
+    needReset.value = false;
+    stepsCount.value = 0;
+}
+
+async function pauseOrResume() {
+    if (paused.value) {
+        stepByStep.value = false;
+        console.log('resume');
+    }
+
+    paused.value = !paused.value;
+}
+
+async function breakPoint() {
+    if (paused.value) {
+        await waitUntil(() => !paused.value);
+    }
+    if (stepByStep.value) {
+        paused.value = true;
+    }
+}
+
+function onResetClicked() {
+    if (started.value) {
+        needReset.value = true;
+        paused.value = false;
+    } else {
+        reset();
+    }
+}
+
 async function selectionSort() {
+    if (started.value) {
+        return;
+    }
+    started.value = true;
+
     for (let i = 0; i < numbers.value.length; i++) {
         smallest.value = i;
         for (let j = i; j < numbers.value.length; j++) {
+            if (j !== i) {
+                await breakPoint();
+                if (needReset.value) {
+                    reset()
+                    return;
+                }
+            }
+
             current.value = j;
             if (numbers.value[j].num < numbers.value[smallest.value].num) {
-                await sleep(500);
+                await breakPoint();
+                if (needReset.value) {
+                    reset()
+                    return;
+                }
+
+                if (!stepByStep.value) {
+                    await sleep(500);
+                }
+
                 smallest.value = j;
             }
+
+            stepsCount.value++;
             await sleep(500);
+        }
+
+        await breakPoint();
+        if (needReset.value) {
+            reset()
+            return;
         }
 
         // swap order for animation
         [numbers.value[i].order, numbers.value[smallest.value].order] = [numbers.value[smallest.value].order, numbers.value[i].order];
+
         await sleep(500);
-        
+
         // disable animation
         showSwapAnimation.value = false;
 
@@ -58,29 +137,57 @@ async function selectionSort() {
 
         // enable animation
         showSwapAnimation.value = true;
+
+        await breakPoint();
+        if (needReset.value) {
+            reset()
+            return;
+        }
     }
+
+    started.value = false;
 }
 </script>
 
 <template>
-    <div style="text-align: initial;" class="flex">
+    <div style="text-align: initial;" class="flex gap-2">
         <div>
             <div :style="{ transform: `translateY(${current * 3}rem)` }"
-                class="h-8 mb-4 b-white b-1px b-solid box-border bg-black flex items-center px-4 transition-transform duration-250ms ease-out">
+                class="items w-25 transition-transform duration-250ms ease-out">
                 Current: {{ numbers[current].num }}</div>
         </div>
         <div>
             <div :style="{ transform: `translateY(${smallest * 3}rem)` }"
-                class="h-8 mb-4 b-white b-1px b-solid box-border bg-black flex items-center px-4 transition-transform duration-250ms ease-out">
+                class="items w-30 transition-transform duration-250ms ease-out">
                 Smallest: {{ numbers[smallest].num }}</div>
         </div>
         <div class="h-120">
-            <div v-for="(n, i) in numbers"
-                :style="{ width: `${n.num / 4}rem`, transform: `translateY(${n.order * 3}rem)` }" :class="{
+            <div v-for="n in numbers" :style="{ width: `${n.num / 4}rem`, transform: `translateY(${n.order * 3}rem)` }"
+                :class="{
                     'transition-transform duration-250ms ease-out': showSwapAnimation,
-                }" class="absolute h-8 box-border b-white b-1px b-solid bg-black flex items-center pl-4">
+                }" class="absolute items">
                 {{ n.num }}</div>
         </div>
     </div>
-    <div class="h-8 mb-4" @click="selectionSort">Start Sorting</div>
+    <div class="h-1px bg-black mb-4 @dark:bg-white"></div>
+    <div class="flex justify-center gap-4">
+        <div class="items">Steps count: {{ stepsCount }}</div>
+        <div @click="selectionSort" class="items" v-if="!started">Start Sorting</div>
+        <div @click="generateRandomNumbers();reset()" class="items" v-if="!started">Regenerate</div>
+        <div @click="pauseOrResume" class="items" v-if="started">{{ paused ? 'Resume' : 'Pause' }}</div>
+        <div @click="stepByStep = true; paused = false" class="items" v-if="paused">Next step</div>
+        <div @click="onResetClicked" class="items" v-if="paused || !started">Reset</div>
+    </div>
 </template>
+
+<style scoped>
+.items {
+    --at-apply: h-8 bg-white flex items-center px-4 text-black b-1 b-solid b-black;
+}
+
+@media screen and (prefers-color-scheme: dark) {
+    .items {
+        --at-apply: bg-black text-white b-white;
+    }
+}
+</style>
